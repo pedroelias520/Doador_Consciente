f9b6ca7afd094557b619c34356897a0a995e9658
Pedro Elias <epedro520@gmail.com>
2020-09-02 22:40:46 -0300

    upgrade:new front-end style

--- doador_consciente/lib/ui/login_screen.dart
+++ doador_consciente/lib/ui/login_screen.dart
@@ -15,7 +15,6 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-
   ContactHelper helper = ContactHelper();
   List<Contact> contacts = List();
   String login_user, senha_user;
@@ -24,11 +23,12 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-
-    void _onSucess(){
-      Navigator.push(context, MaterialPageRoute(builder: (context) => ChooseScreen()));
+    void _onSucess() {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => ChooseScreen()));
     }
-    void _onFail(){
+
+    void _onFail() {
       showDialog(
           context: context,
           builder: (BuildContext context) {
@@ -45,127 +45,148 @@ class _LoginPageState extends State<LoginPage> {
             );
           });
     }
-    return Scaffold(
-      backgroundColor: Colors.red,
-      bottomNavigationBar: FlatButton(
-          color: Colors.redAccent,
-          onPressed: () {
-            showCadastroPage(context: context);
-          },
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                "Não tem uma conta ?",
-                style: TextStyle(color: Colors.white70, fontFamily: 'UbuntuB'),
-              ),
-              Text(
-                "Cadastre-se",
-                style: TextStyle(
-                    color: Colors.white, fontSize: 15, fontFamily: 'UbuntuB'),
-              )
-            ],
-          )),
-      body: ScopedModelDescendant<UserModel>(
-        builder: (context,builder,model){
-          if(model.isLodding)
-            return Center(child: CircularProgressIndicator(backgroundColor: Colors.red,),);
-          return Stack(children: <Widget>[
-            SingleChildScrollView(
-              padding: EdgeInsets.only(left: 10.0, right: 10.0),
-              child: Center(
-                heightFactor: 1.6,
-                child: Form(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: <Widget>[
-                        Icon(
-                          Icons.favorite_border,
-                          size: 140.0,
-                          color: Colors.white,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: Text(
-                            "Doador Consciente",
-                            style: TextStyle(
-                                fontFamily: "Delighter",
-                                fontSize: 40,
-                                color: Colors.white),
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: TextFormField(
-                            controller: loginController,
-                            keyboardType: TextInputType.emailAddress,
-                            decoration: InputDecoration(
-                              icon: Icon(
-                                Icons.person,
-                                color: Colors.white,
-                              ),
-                              border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(20.0)),
-                              labelText: "Login",
-                              hintStyle: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 60.0,
-                                  fontFamily: 'UbuntuB'),
-                            ),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: TextFormField(
-                            controller: senhaController,
-                            keyboardType: TextInputType.visiblePassword,
-                            decoration: InputDecoration(
-                              icon: Icon(
-                                Icons.lock_outline,
-                                color: Colors.white,
-                              ),
-                              border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(20.0)),
-                              labelText: "Senha",
-                              hintStyle: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 60.0,
-                                  fontFamily: 'UbuntuB'),
-                            ),
-                          ),
-                        ),
-                        Container(
-                          width: 80.0,
-                          child: Padding(
-                            padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
-                            child: RaisedButton(
-                              splashColor: Colors.redAccent,
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(20.0)),
-                              onPressed: () {
-                                    model.SignIn(loginController.text, senhaController.text, _onSucess, _onFail);
-                              },
-                              child: Text(
-                                "Logar",
-                                style: TextStyle(
-                                    color: Colors.grey,
-                                    fontSize: 22.0,
-                                    fontFamily: "UbuntuB"),
-                              ),
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                      ],
-                    )),
-              ),
-            )
-          ]);
-        },
-      )
-    );
 
+    return Scaffold(
+        backgroundColor: Colors.white,
+        bottomNavigationBar: FlatButton(
+            color: Color.fromRGBO(255, 0, 0, 1),
+            onPressed: () {
+              showCadastroPage(context: context);
+            },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  "Não tem uma conta ?",
+                  style:
+                      TextStyle(color: Colors.white70, fontFamily: 'UbuntuB'),
+                ),
+                Text(
+                  "Cadastre-se",
+                  style: TextStyle(
+                      color: Colors.white, fontSize: 15, fontFamily: 'UbuntuB'),
+                )
+              ],
+            )),
+        body: ScopedModelDescendant<UserModel>(
+          builder: (context, builder, model) {
+            if (model.isLodding)
+              return Center(
+                child: CircularProgressIndicator(
+                  backgroundColor: Colors.red,
+                ),
+              );
+            return Stack(children: <Widget>[
+              SingleChildScrollView(
+                padding: EdgeInsets.only(left: 10.0, right: 10.0),
+                child: Center(
+                  heightFactor: 1.6,
+                  child: Form(
+                      child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Icon(
+                            Icons.favorite_border,
+                            size: 90.0,
+                            color: Colors.red,
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Text(
+                          "Seja bem vindo,",
+                          style: TextStyle(
+                              fontFamily: "UbuntuB",
+                              fontSize: 35,
+                              color: Color.fromRGBO(255, 0, 0, 1)),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Text(
+                          "Insira suas credenciais",
+                          style: TextStyle(
+                              fontFamily: "UbuntuB",
+                              fontSize: 20,
+                              color: Color.fromRGBO(253, 148, 148, 1)),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            top: 50, left: 8.0, right: 8.0),
+                        child: TextFormField(
+                          controller: loginController,
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: InputDecoration(
+                            icon: Icon(
+                              Icons.person,
+                              color: Colors.red,
+                            ),
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0)),
+                            labelText: "Login",
+                            hintStyle: TextStyle(
+                                color: Colors.red,
+                                fontSize: 60.0,
+                                fontFamily: 'UbuntuB'),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            top: 20, left: 8.0, right: 8.0),
+                        child: TextFormField(
+                          controller: senhaController,
+                          keyboardType: TextInputType.visiblePassword,
+                          decoration: InputDecoration(
+                            icon: Icon(
+                              Icons.lock_outline,
+                              color: Colors.red,
+                            ),
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0)),
+                            labelText: "Senha",
+                            hintStyle: TextStyle(
+                                color: Colors.white,
+                                fontSize: 60.0,
+                                fontFamily: 'UbuntuB'),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        child: Padding(
+                          padding: EdgeInsets.only(
+                              top: 10.0, left: 20.0, right: 20.0),
+                          child: RaisedButton(
+                            splashColor: Colors.redAccent,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(5.0)),
+                            onPressed: () {
+                              model.SignIn(loginController.text,
+                                  senhaController.text, _onSucess, _onFail);
+                            },
+                            child: Text(
+                              "Logar",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20.0,
+                                  fontFamily: "UbuntuB"),
+                            ),
+                            color: Color.fromRGBO(255, 0, 0, 1),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )),
+                ),
+              )
+            ]);
+          },
+        ));
   }
 
   void showCadastroPage({Contact contact, BuildContext context}) async {
@@ -182,8 +203,7 @@ class _LoginPageState extends State<LoginPage> {
     if (loginController.text == "BalterCFH" && senhaController.text == "123") {
       await Navigator.push(
         context,
-        MaterialPageRoute(
-            builder: (context) => ChooseScreen()),
+        MaterialPageRoute(builder: (context) => ChooseScreen()),
       );
     } else {
       showDialog(
@@ -197,4 +217,3 @@ class _LoginPageState extends State<LoginPage> {
     }
   }
 }
-
